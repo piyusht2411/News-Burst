87c4c69feeb7a21b5306486f90a5cfee8293e39c
Piyush Thakur <90854455+piyusht2411@users.noreply.github.com>
2023-07-24 16:17:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "9202e8662a67402abc9c9b4ce7edb06f";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
